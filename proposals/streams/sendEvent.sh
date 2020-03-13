249b4ece16f11d742159115c8732f335ea8367ff
Rüdiger zu Dohna <snackbox@sinntr.eu>
2020-03-13 13:40:24 +0100

    remove streams (was promoted to https://github.com/eclipse/microprofile-reactive-messaging) and microprofile-graphql (was promoted to https://github.com/eclipse/microprofile-graphql)
    Signed-off-by: Rüdiger zu Dohna <snackbox@sinntr.eu>

--- proposals/streams/sendEvent.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-topic=$1
-name=$2
-email=$3
-
-echo '{"type": "UPDATED", "id": "6b46e75c-2e1e-4a91-b7a0-ff785c305a01", "name": "'$2'", "email": "'$3'"}' | kafkacat -P -b localhost:9092 -t user-details.events$1
